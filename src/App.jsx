bcc21548506888013e1c12833caf0c9063351387
mohamed-Fathy1 <60184431+mohamed-Fathy1@users.noreply.github.com>
2023-09-01 14:16:18 +0000

    Update portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Mohamed Fathy",
   title: "Web Designer & Content Creator",
   email: "alex@example.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "https://www.linkedin.com/in/mohamed-fathy-8487551ab/",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
